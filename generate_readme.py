07e51c66f60996ff0b60619bcece4c6fc7e9d700
Kentaro Wada <www.kentaro.wada@gmail.com>
2020-04-02 07:04:55 +0100

    Update linters

--- generate_readme.py
+++ generate_readme.py
@@ -10,37 +10,37 @@ import yaml
 
 def get_section(title, data, h=2):
     if not isinstance(data, list):
-        content = '%s %s\n\n' % ('#' * h, title.capitalize())
+        content = "%s %s\n\n" % ("#" * h, title.capitalize())
         for sub_title, sub_data in data.items():
-            content += get_section(sub_title, sub_data, h=h+1)
+            content += get_section(sub_title, sub_data, h=h + 1)
         return content
 
-    headers = ['Numpy', 'PyTorch']
-    keys = ['numpy', 'pytorch']
+    headers = ["Numpy", "PyTorch"]
+    keys = ["numpy", "pytorch"]
     rows = []
     for d in data:
         row = []
         for key in keys:
             if isinstance(d[key], dict):
-                content = d[key]['content']
-                is_code = d[key].get('is_code', True)
+                content = d[key]["content"]
+                is_code = d[key].get("is_code", True)
             elif d[key] is None:
-                content = ''
+                content = ""
                 is_code = False
             else:
                 content = d[key]
                 is_code = True
             if is_code and content:
                 if len(content.splitlines()) > 1:
-                    content = '<pre>\n{}</pre>'.format(content)
+                    content = "<pre>\n{}</pre>".format(content)
                 else:
-                    content = '<code>{}</code>'.format(content)
+                    content = "<code>{}</code>".format(content)
             row.append(content)
         rows.append(row)
 
-    content = '%s %s\n\n' % ('#' * h, title.capitalize())
-    content += tabulate.tabulate(rows, headers=headers, tablefmt='html')
-    content += '\n\n'
+    content = "%s %s\n\n" % ("#" * h, title.capitalize())
+    content += tabulate.tabulate(rows, headers=headers, tablefmt="html")
+    content += "\n\n"
     return content
 
 
@@ -48,30 +48,31 @@ def get_contents():
     # keep order in yaml file
     yaml.add_constructor(
         yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
-        lambda loader, node:
-            collections.OrderedDict(loader.construct_pairs(node)),
+        lambda loader, node: collections.OrderedDict(
+            loader.construct_pairs(node)
+        ),
     )
 
-    yaml_file = osp.join(here, 'conversions.yaml')
+    yaml_file = osp.join(here, "conversions.yaml")
     with open(yaml_file) as f:
         data = yaml.load(f)
     contents = []
     for title, data in data.items():
         section = get_section(title, data)
         contents.append(section)
-    return '\n'.join(contents)
+    return "\n".join(contents)
 
 
 here = osp.dirname(osp.abspath(__file__))
 
 
 def main():
-    with open(osp.join(here, 'README.md.in')) as f:
+    with open(osp.join(here, "README.md.in")) as f:
         template = f.read()
     template = string.Template(template)
     readme = template.substitute(contents=get_contents())
     print(readme)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- run_tests.py
+++ run_tests.py
@@ -20,9 +20,9 @@ def parse(data):
                 for key in item:
                     item_k = item[key]
                     if isinstance(item_k, dict):
-                        content = item_k['content']
-                        is_code = item_k.get('is_code', True)
-                        if item_k.get('skip_test', False):
+                        content = item_k["content"]
+                        is_code = item_k.get("is_code", True)
+                        if item_k.get("skip_test", False):
                             continue
                     else:
                         content = item_k
@@ -34,47 +34,47 @@ def parse(data):
 def main():
     here = osp.dirname(osp.abspath(__file__))
 
-    with open(osp.join(here, 'conversions.yaml')) as f:
+    with open(osp.join(here, "conversions.yaml")) as f:
         data = yaml.load(f)
 
-    for fname in glob.glob(osp.join(here, 'tests/*.py')):
+    for fname in glob.glob(osp.join(here, "tests/*.py")):
         os.remove(fname)
 
     for i, (key, content) in enumerate(parse(data)):
-        if key == 'numpy':
-            code = '''\
+        if key == "numpy":
+            code = """\
 import numpy as np
 
 
 def test_{key}_{id:04d}():
     x = np.array([[1, 2, 3], [4, 5, 6]])
 {content}
-'''
-        elif key == 'pytorch':
-            code = '''\
+"""
+        elif key == "pytorch":
+            code = """\
 import torch
 
 
 def test_{key}_{id:04d}():
     x = torch.tensor([[1, 2, 3], [4, 5, 6]])
 {content}
-'''
+"""
         else:
             raise ValueError
 
-        content = '\n'.join(' ' * 4 + l for l in content.splitlines())
+        content = "\n".join(" " * 4 + l for l in content.splitlines())
         code = code.format(key=key, id=i, content=content)
 
         test_file = osp.join(
-            here, 'tests/test_{key}_{id:04d}.py'.format(key=key, id=i)
+            here, "tests/test_{key}_{id:04d}.py".format(key=key, id=i)
         )
-        with open(test_file, 'w') as f:
+        with open(test_file, "w") as f:
             f.write(code)
 
-    cmd = 'pytest -vs tests'
-    print('+ %s' % cmd)
+    cmd = "pytest -vs tests"
+    print("+ %s" % cmd)
     subprocess.call(cmd, shell=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
